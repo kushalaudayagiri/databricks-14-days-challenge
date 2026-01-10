075eb5429d7f85a937ee67a7cebb7770e2d35e3b
kushalaudayagiri <160637292+kushalaudayagiri@users.noreply.github.com>
2026-01-10 17:42:57 +0530

    Add basic PySpark DataFrame creation and filtering

---
+++ Day-01/day1_pyspark_basics.py
@@ -0,0 +1,7 @@
+# Create simple DataFrame
+data = [("iPhone", 999), ("Samsung", 799), ("MacBook", 1299)]
+df = spark.createDataFrame(data, ["product", "price"])
+df.show()
+
+# Filter expensive products
+df.filter(df.price > 1000).show()
